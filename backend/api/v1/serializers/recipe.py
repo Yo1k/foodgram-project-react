334264a2cad3610be5a5e8a9c44a7c9b1fc4d3ae
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2023-02-24 18:31:41 +0300

    Implement `update` in `recipes` endpoint

--- backend/api/v1/serializers/recipe.py
+++ backend/api/v1/serializers/recipe.py
@@ -1,12 +1,8 @@
-import sys
-
-from django.core.exceptions import ValidationError
-from django.http import Http404
 from recipes.models import Ingredient, IngredientAmount, Recipe, Tag
 from rest_framework import serializers
-from rest_framework.generics import get_object_or_404
 from users.v1.serializers import CustomUserSerializer
 from .tags import TagSerializer
+
 
 class IngredientInRecipeCreateSerializer(
     serializers.HyperlinkedModelSerializer
@@ -72,48 +68,47 @@ class RecipeCreateUpdate(serializers.ModelSerializer):
         read_only_fields = (
             'id',
         )
-    # SKTODO переделать на exists()
-    def __save_related_ingredients(self, ingredients_data, obj):
+    
+    def __check_related_ingredients(self, ingredients_data):
         if not ingredients_data:
             return None
-        
-        
-        # ingredient_ids = Ingredient.objects.all().values_list('id', flat=True)
-        # for ingredient in ingredients_data:
-        #     if ingredient['id'] not in ingredient_ids:
-        #         raise serializers.ValidationError(
-        #             f'There is no ingrediet with id={ingredient["id"]}'
-        #         )
-        try:
-            for ingrediet in ingredients_data:
-                ingredient_id = ingrediet['id']
-                get_object_or_404(
-                    Ingredient,
-                    id=ingredient_id
+        ingredients_ids = Ingredient.objects.all().values_list('id', flat=True)
+        for ingredient in ingredients_data:
+            if ingredient['id'] not in ingredients_ids:
+                raise serializers.ValidationError(
+                    f'There is no ingrediet with id={ingredient["id"]}'
                 )
-        except Http404:
-            raise serializers.ValidationError(
-                f'There is no ingrediet with id={ingredient_id}'
-            )
-        
-        for ingredient_data in ingredients_data:
-            obj.ingredients.add(
-                ingredient_data['id'],
-                through_defaults={'amount': ingredient_data['amount']}
-            )
+    
+    def __check_related_tags(self, tags_data):
+        if not tags_data:
+            return None
+        tags_ids = Tag.objects.all().values_list('id', flat=True)
+        for tag in tags_data:
+            if tag.id not in tags_ids:
+                raise serializers.ValidationError(
+                    f'There is no tag with id={tag["id"]}'
+                )
+
+    def __save_related_ingredients(self, ingredients_data, obj, update=False):
+        if not ingredients_data:
+            return None
+
+        if not update:
+            for ingredient_data in ingredients_data:
+                obj.ingredients.add(
+                    ingredient_data['id'],
+                    through_defaults={'amount': ingredient_data['amount']}
+                )
+        else:
+            obj.ingredientamount_set.all().delete()
+            for ingredient_data in ingredients_data:
+                obj.ingredients.add(
+                    ingredient_data['id'],
+                    through_defaults={'amount': ingredient_data['amount']}
+                )
 
     def __save_related_tags(self, tags_data, obj):
         if tags_data:
-            try:
-                for tag in tags_data:
-                    get_object_or_404(
-                        Tag,
-                        id=tag.id
-                    )
-            except Http404:
-                raise serializers.ValidationError(
-                    f'There is no tag with id={tag.id}'
-                )
             obj.tags.set(tags_data)
 
     def create(self, validated_data):
@@ -122,23 +117,40 @@ class RecipeCreateUpdate(serializers.ModelSerializer):
 
         recipe = Recipe(
             author=self.context.get('request').user,
-            # ingredients=self.__get_ingredients(ingredients_data), SKTODO
             **validated_data
         )
-        try:
-            recipe.full_clean()
-            # SKTODO проверки, что ингредиенты и тэги сущесвуют, после сохрн.
-            recipe.save()
-            self.__save_related_ingredients(ingredients_data, recipe)
-            self.__save_related_tags(tags_data, recipe)
-            return recipe
+        recipe.full_clean()
+        self.__check_related_ingredients(ingredients_data)
+        self.__check_related_tags(tags_data)
+        recipe.save()
+        self.__save_related_ingredients(ingredients_data, recipe)
+        self.__save_related_tags(tags_data, recipe)
+        return recipe
 
-        except (ValidationError, serializers.ValidationError) as e:
-            raise e #serializers.ValidationError(e)
     
-    # def update(self, instance, validated_data):
-    #     ...
+    def update(self, instance, validated_data):
+        ingredients_data = validated_data.pop('ingredientamount_set', None)
+        tags_data = validated_data.pop('tags', None)
 
+        self.__check_related_ingredients(ingredients_data)
+        self.__check_related_tags(tags_data)
+
+        #SKTODO add image
+        # instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.pop('name', instance.name)
+        instance.text = validated_data.pop('text', instance.text)
+        instance.cooking_time = validated_data.pop(
+            'cooking_time', instance.cooking_time
+        )
+        instance.save()
+        self.__save_related_ingredients(
+            ingredients_data,
+            instance,
+            update=True
+        )
+        self.__save_related_tags(tags_data, instance)
+
+        return instance
 
 class RecipeListSerializer(serializers.ModelSerializer):
     author = CustomUserSerializer(read_only=True)
--- backend/api/v1/views/recipe.py
+++ backend/api/v1/views/recipe.py
@@ -1,9 +1,6 @@
-import sys
 from recipes.models import Recipe
-from users.models import Subscription
-from rest_framework import mixins, status, viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
-from django.db.models import Exists, OuterRef
 
 from ..serializers import RecipeCreateUpdate, RecipeListSerializer
 
@@ -13,11 +10,9 @@ class RecipeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Recipe.objects.all().prefetch_related('author__subscribing')
 
 
-class RecipeViewSet(
-    viewsets.ModelViewSet
-):
-    queryset = Recipe.objects.all().prefetch_related('author__subscribing')
+class RecipeViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
+    queryset = Recipe.objects.all().prefetch_related('author__subscribing')
 
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
@@ -28,8 +23,8 @@ class RecipeViewSet(
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # Uses this to preserve the original structure of the `ViewSet` class
-        # and get `obj` that is saved to DB. 
+        # Uses this to preserve the original structure
+        # of the `ModelViewSet` class and get `obj` that is saved to DB. 
         obj = self.perform_create(serializer)
 
         # Changing serializer that is used for `Response` formation
@@ -43,3 +38,26 @@ class RecipeViewSet(
     
     def perform_create(self, serializer):
         return serializer.save()
+
+    def perform_update(self, serializer):
+        return serializer.save()
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        # self.perform_update(serializer) SKTODO old
+        # Uses this to preserve the original structure
+        # of the `ModelViewSet` class and get `obj` that is saved to DB. 
+        obj = self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        # Changing serializer that is used for `Response` formation
+        serializer = RecipeListSerializer(obj, context={'request': request})
+        return Response(serializer.data)
